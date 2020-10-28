31f5c99c9cb43a280037e1dd3605e0745ffb1e6d
kalavh <jefferson_1996Th@hotmail.com>
2020-10-28 18:51:12 -0300

    up migrate structre

--- src/migrations/20201028213720-create-users.js
+++ src/migrations/20201028213720-create-users.js
@@ -2,12 +2,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+       }
+     })
   },
 
   down: async (queryInterface, Sequelize) => {
