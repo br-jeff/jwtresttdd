a8a9f09839dec338b84fe11c1a062227415675c3
kalavh <jefferson_1996Th@hotmail.com>
2020-10-28 20:55:56 -0300

    test configurations

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,10 @@
+const { DESCRIBE } = require("sequelize/types/lib/query-types")
+
+describe('Authentication' , () => {
+    it('should recive JWT token when authenticated with valid credentials' , () => {
+
+    })
+
+})
+
+
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const routes = require('express').Router()
+const { User } = require('./app/models/user') 
 
+User.create({name:'teste', email: 'teste@teste.com', passwrod_hash: "1213124" })
 
 module.exports = routes
