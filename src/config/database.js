adb10cfb558c2d6005c0d2cc6abe01c2d4140b4c
kalavh <jefferson_1996Th@hotmail.com>
2020-10-28 18:38:01 -0300

    config of node and first  migration

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    username: "root",
+    password : 'root',
+    database : "teste",
+    host : "127.0.0.1",
+    dialect : "mysql",
+    operatorsAliases : false,
+    loggin: false,
+    define: {
+      timestamps: true,
+    }
+  },
+ 
+}
